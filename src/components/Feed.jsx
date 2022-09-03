1f908fdc7ba856c3e0cbcd9234088daff0f30ccb
Amandeep <singh.amandeep5665@gmail.com>
2022-09-04 01:11:01 +0530

    making sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { useState,useEffect } from 'react'
+import { Box, Stack, Typography} from '@mui/material'
+import { borderRight } from '@mui/system'
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{sx:"column",
+  md:"row"}}}>
+    <Box sx ={{height:{sx:'auto',md:'92vh'},
+  borderRight:'1px solid #3d3d3d', px:{sx:0,md:2}}}>
+      Sidebar
+    </Box>
+  </Stack>
   )
 }
 
