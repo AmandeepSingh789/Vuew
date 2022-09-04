130e21e5172d81508c564fad8fbf10ac7b841c79
Amandeep <singh.amandeep5665@gmail.com>
2022-09-05 00:54:14 +0530

    made sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { categories } from './constants'
+
+const selectedCategory='New'
+const Sidebar = () => {
+  return (
+    <Stack direction="row"
+    sx={{
+        overflowY:'auto',
+        height:{sx:"auto", md:'95%'},
+        flexDirection:{md:'column'},}} >
+        {categories.map((category)=>(
+            <button className='category-btn'
+            style={{
+                background:category.name===selectedCategory && 'linear-gradient(286deg, rgba(0,74,173,1) 0%, rgba(0,74,173,1) 0%, rgba(2,130,162,1) 49%, rgba(3,152,158,1) 68%)',
+                color:'white'
+            }} key={category.name}>
+
+                <span style={{ color: category.name===selectedCategory ? 'white' :
+                '#0282a2'
+                , marginRight:'15px' }}>
+                    
+                    {category.icon}</span> 
+                <span style={{opacity:category.name===selectedCategory ? '1' :
+                '0.8' }}>{category.name}</span>
+            </button>
+        ))}
+        </Stack>
+  )
+}
+
+export default Sidebar
---
+++ src/components/constants.js
@@ -0,0 +1,34 @@
+import LibraryMusicTwoToneIcon from '@mui/icons-material/LibraryMusicTwoTone';
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import CodeTwoToneIcon from '@mui/icons-material/CodeTwoTone';
+import OndemandVideoTwoToneIcon from '@mui/icons-material/OndemandVideoTwoTone';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvTwoToneIcon from '@mui/icons-material/LiveTvTwoTone';
+import SchoolTwoToneIcon from '@mui/icons-material/SchoolTwoTone';
+import FaceRetouchingNaturalTwoToneIcon from '@mui/icons-material/FaceRetouchingNaturalTwoTone';
+import CheckroomTwoToneIcon from '@mui/icons-material/CheckroomTwoTone';
+import GraphicEqTwoToneIcon from '@mui/icons-material/GraphicEqTwoTone';
+import TheaterComedyTwoToneIcon from '@mui/icons-material/TheaterComedyTwoTone';
+import FitnessCenterTwoToneIcon from '@mui/icons-material/FitnessCenterTwoTone';
+import DeveloperModeTwoToneIcon from '@mui/icons-material/DeveloperModeTwoTone';
+import ScoreboardTwoToneIcon from '@mui/icons-material/ScoreboardTwoTone';
+
+
+export const categories = [
+    { name: 'New', icon: <HomeTwoToneIcon />, },
+    { name: 'Coding', icon: <CodeTwoToneIcon />, },
+    { name: 'ReactJS', icon: <CodeTwoToneIcon />, },
+    { name: 'NextJS', icon: <CodeTwoToneIcon />, },
+    { name: 'Music', icon: <LibraryMusicTwoToneIcon /> },
+    { name: 'Education', icon: <SchoolTwoToneIcon />, },
+    { name: 'Podcast', icon: <GraphicEqTwoToneIcon />, },
+    { name: 'Movie', icon: <OndemandVideoTwoToneIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvTwoToneIcon />, },
+    { name: 'Sport', icon: <ScoreboardTwoToneIcon />, },
+    { name: 'Fashion', icon: <CheckroomTwoToneIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalTwoToneIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyTwoToneIcon />, },
+    { name: 'Gym', icon: <FitnessCenterTwoToneIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeTwoToneIcon />, },
+  ];
