c81b73b35e9d69d51ba1e28a3cef403f94c48fa4
Amandeep <singh.amandeep5665@gmail.com>
2022-09-06 00:18:02 +0530

    channel detail page done

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,40 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import {Videos,ChannelCard} from'./'
 
 const ChannelDetail = () => {
+  const {id} =useParams();
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=>{
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data)=>setChannelDetail(data?.items[0]) );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=>setVideos(data?.items) )
+  },[id])
   return (
-    <div>ChannelDetail</div>
+
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background:'linear-gradient(286deg, rgba(0,74,173,1) 0%, rgba(0,74,173,1) 0%, rgba(2,130,162,1) 49%, rgba(3,152,158,1) 68%)',
+          zIndex:10,
+          height:'300px'
+        }} >
+        </div>
+        <ChannelCard channelDetail={channelDetail} marginTop ='-110px'/>
+      </Box>
+      <Box display="flex" p='2'>
+          <Box sx={{mr:{sm:'100px'}}} />
+              <Videos videos={videos} />
+          
+      </Box>
+      
+    </Box>
   )
 }
 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,12 +8,12 @@ const VideoCard = ({ video: {id:{
     videoId
 }, snippet} } ) => {
   return (
-    <Card sx={{ width:{md:'320px', xs:'100%'},
+    <Card sx={{ width:{md:'320px', sm: '358px', xs:'100%'},
     boxShadow:'none', borderRadius:0}}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
         alt={snippet?.title}
-        sx={{width:358, height:180}}
+        sx={{width: { xs: '100%', sm: '358px'}, height:180}}
 
         image={snippet?.thumbnails?.high?.url} />
         </Link>
