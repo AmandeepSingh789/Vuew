968c8e76c9a377a785ccee09b142f98ae267f939
Amandeep <singh.amandeep5665@gmail.com>
2022-09-06 00:22:05 +0530

    started search feed page

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { Box, Stack, Typography} from '@mui/material'
+import { borderRight } from '@mui/system'
+import {  Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const SearchFeed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>(
+      setVideos(data.items)
+    ))
+  },[selectedCategory])
+
+
   return (
-    <div>SearchFeed</div>
+    <Stack sx={{flexDirection:{sx:"column",
+  md:"row"}}}>
+    
+    <Box p={2} sx={{overflowY:'auto',height:'90vh ' ,flex:2}}>
+    <Typography variant ='h4' fontWeight='bold' mb={2}sx={{color:'white'}}>
+      {selectedCategory}
+      <span style ={{color:'#0282a2',marginLeft:'8px'}}>videos</span>
+    </Typography>
+
+    <Videos videos={videos} />
+    
+    </Box>
+  </Stack>
   )
 }
 
