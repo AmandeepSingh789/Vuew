a3ec2edd64028bda9d2de3b4b251ace50dc5c987
Amandeep <singh.amandeep5665@gmail.com>
2022-09-06 00:29:47 +0530

    search working

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -4,34 +4,32 @@ import { Box, Stack, Typography} from '@mui/material'
 import { borderRight } from '@mui/system'
 import {  Videos } from './'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { useParams } from 'react-router-dom'
+
 
 const SearchFeed = () => {
 
-  const [selectedCategory, setSelectedCategory] = useState('New');
-
+  const {searchTerm} =useParams();
+  
   const [videos, setVideos] = useState([])
 
   useEffect(()=>{
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>(
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data)=>(
       setVideos(data.items)
     ))
-  },[selectedCategory])
+  },[searchTerm])
 
 
   return (
-    <Stack sx={{flexDirection:{sx:"column",
-  md:"row"}}}>
-    
     <Box p={2} sx={{overflowY:'auto',height:'90vh ' ,flex:2}}>
     <Typography variant ='h4' fontWeight='bold' mb={2}sx={{color:'white'}}>
-      {selectedCategory}
-      <span style ={{color:'#0282a2',marginLeft:'8px'}}>videos</span>
+      Search Results for: 
+      <span style ={{color:'#0282a2',marginLeft:'8px'}}>{searchTerm}</span>
     </Typography>
 
     <Videos videos={videos} />
     
     </Box>
-  </Stack>
   )
 }
 
