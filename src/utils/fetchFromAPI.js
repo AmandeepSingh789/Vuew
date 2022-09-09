d6cddcd765f212dd4a31a2724acc7c653c6c7a58
Amandeep <singh.amandeep5665@gmail.com>
2022-09-09 14:24:14 +0530

    maybe will work

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-import express from 'express'
 
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
